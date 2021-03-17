0a22c4315a31cfec3b3d96ea6f5bdd227f7edb59
rodriguesporan <rodriguesporan@gmail.com>
2021-03-17 13:27:40 -0300

    feat: using View Binding to access more easily the views

--- app/src/main/java/com/example/emptyactivity/MainActivity.kt
+++ app/src/main/java/com/example/emptyactivity/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.emptyactivity
 
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.emptyactivity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var btnShowMsg = findViewById<Button>(R.id.show_msg)
-        btnShowMsg.setOnClickListener { _ ->
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.showMsg.setOnClickListener { _ ->
             Toast.makeText(this, "Hello World", Toast.LENGTH_LONG).show()
         }
     }
