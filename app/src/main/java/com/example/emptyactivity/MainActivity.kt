da3d9bdfd2205ca1bde44a01d3d31958841b3e9c
rodriguesporan <rodriguesporan@gmail.com>
2021-03-18 11:21:14 -0300

    feat: added a implicit intent, a viewmodel and a observer to observe the livedata which updates the textview

--- app/src/main/java/com/example/emptyactivity/MainActivity.kt
+++ app/src/main/java/com/example/emptyactivity/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.emptyactivity
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.example.emptyactivity.databinding.ActivityMainBinding
+import com.example.emptyactivity.model.AppViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val model: AppViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.showMsg.setOnClickListener { _ ->
-            Toast.makeText(this, "Hello World", Toast.LENGTH_LONG).show()
+        model.content.observe(this, Observer<String> { newString ->
+            binding.txtView.text = newString
+        })
+
+        binding.showMsg.setOnClickListener {
+            model.setContent("By World")
+            Toast.makeText(this, model.content.value, Toast.LENGTH_LONG).show()
+        }
+
+        binding.sendEmail.setOnClickListener {
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "André")
+                putExtra(Intent.EXTRA_EMAIL, "andre@gmail")
+                putExtra(Intent.EXTRA_SUBJECT, "You have been hired")
+                type = "text/plain"
+            }
+
+            try {
+                startActivity(sendIntent)
+            } catch (e: ActivityNotFoundException) {
+                Log.d("INTENT", e.toString())
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/emptyactivity/model/AppViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.emptyactivity.model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AppViewModel: ViewModel() {
+    private val _content = MutableLiveData<String>("Hello World")
+    val content: LiveData<String> get() = _content
+
+    fun setContent(newContent: String) {
+        _content.value = newContent
+    }
+}
