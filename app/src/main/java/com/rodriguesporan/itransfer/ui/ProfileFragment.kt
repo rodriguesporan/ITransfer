9fe662fa05b5bb7717bb0002976f808ffb8370ac
rodriguesporan <rodriguesporan@gmail.com>
2021-05-03 19:31:55 -0300

    fix: fixed the logout bug

--- app/src/main/java/com/rodriguesporan/itransfer/ui/ProfileFragment.kt
+++ app/src/main/java/com/rodriguesporan/itransfer/ui/ProfileFragment.kt
@@ -1,39 +1,93 @@
 package com.rodriguesporan.itransfer.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.android.material.button.MaterialButton
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.rodriguesporan.itransfer.R
+import com.rodriguesporan.itransfer.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var _binding: FragmentProfileBinding? = null
+    private val binding: FragmentProfileBinding get() = _binding!!
 
-        auth = Firebase.auth
-
-    }
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        val signOutButton = container?.findViewById<MaterialButton>(R.id.sign_out_button)
-        signOutButton?.setOnClickListener {
-            signOut()
-        }
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply { signOutButton.setOnClickListener { signOut() } }
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
     }
 
     private fun signOut() {
-        Firebase.auth.signOut()
+        // Firebase sign out
+        auth.signOut()
+
+        // Google sign out
+        googleSignInClient.signOut().addOnCompleteListener(requireActivity()) {
+            updateUI(null)
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            binding.apply {
+                displayNameTextView.text = auth.currentUser.displayName
+                emailTextView.text = auth.currentUser.email
+                idTextView.text = auth.currentUser.uid
+            }
+        } else {
+            binding.apply {
+                displayNameTextView.text = ""
+                emailTextView.text = ""
+                idTextView.text = ""
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        private const val TAG = "ITransfer"
     }
 }
